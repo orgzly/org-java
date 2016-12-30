a2047512983cef722f6c8e59474dd4b06c0895a3
Neven <neven@orgzly.com>
2016-12-30 12:50:03 -0500

    Create ArrayListSpaceSeparated for state keywords

---
+++ src/main/java/com/orgzly/org/utils/ArrayListSpaceSeparated.java
@@ -0,0 +1,26 @@
+package com.orgzly.org.utils;
+
+import com.orgzly.org.OrgStringUtils;
+
+import java.util.ArrayList;
+
+public class ArrayListSpaceSeparated extends ArrayList<String> {
+    private final static String DELIMITER = " ";
+
+    public ArrayListSpaceSeparated() {
+        super();
+    }
+
+    public ArrayListSpaceSeparated(String str) {
+        for (String s: str.split(DELIMITER)) {
+            String st = s.trim();
+            if (st.length() > 0) {
+                add(st);
+            }
+        }
+    }
+
+    public String toString() {
+        return OrgStringUtils.join(this, DELIMITER);
+    }
+}
