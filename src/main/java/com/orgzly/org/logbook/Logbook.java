c9e11b09410e83cefd7970fe01d28a0f8c050a66
Jethro Kuan <jethrokuan95@gmail.com>
2018-01-08 05:12:41 +0800

    add docstring

--- src/main/java/com/orgzly/org/logbook/Logbook.java
+++ src/main/java/com/orgzly/org/logbook/Logbook.java
@@ -4,6 +4,18 @@ import com.orgzly.org.logbook.LogbookEntry;
 
 import java.util.LinkedList;
 
+/**
+ * The internal representation for a LOGBOOK drawer.
+ *
+ * Logbook entries are stored within a linked list, in chronological order.
+ * That is, more recent entries are located at the tail of the list.
+ *
+ * This is the opposite of how Org handles logbook entries, where
+ * more recent entries are at the start of the drawer. Hence, when
+ * converting the LOGBOOK into its textual representation,
+ * it is important to reverse the order.
+ *
+ */
 public class Logbook {
     LinkedList<LogbookEntry> entries;
 
