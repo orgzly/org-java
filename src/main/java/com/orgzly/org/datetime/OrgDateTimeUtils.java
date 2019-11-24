cd3cdaff59633ef8246866aa9ed763db15ff3144
Neven <neven@orgzly.com>
2019-11-24 11:42:15 +0100

    Update joda-time and annotations versions

--- src/main/java/com/orgzly/org/datetime/OrgDateTimeUtils.java
+++ src/main/java/com/orgzly/org/datetime/OrgDateTimeUtils.java
@@ -32,6 +32,7 @@ public class OrgDateTimeUtils {
          * @param fromTime Inclusive
          * @param beforeTime Exclusive. Can be null in which case limit has to be specified
          * @param useRepeater Use repeater from {@link OrgDateTime}
+         * @param warningPeriod Deadline warning period
          * @param limit When {@code orgTime} has a repeater, limit the number of results to this number
          *
          * @return List of times within specified interval
